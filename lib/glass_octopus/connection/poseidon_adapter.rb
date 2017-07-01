c224f41255700834941921d3479c30f3d17b0788
Tamás Michelberger <tomi@secretsaucepartners.com>
2017-07-01 18:41:22 +0200

    Add documentation for PoseidonAdapter

--- lib/glass_octopus/connection/poseidon_adapter.rb
+++ lib/glass_octopus/connection/poseidon_adapter.rb
@@ -3,7 +3,18 @@ require "poseidon_cluster"
 require "glass_octopus/message"
 
 module GlassOctopus
+  # Connection adapter that uses the {https://github.com/bpot/poseidon poseidon
+  # gem} to talk to Kafka 0.8.x. Tested with Kafka 0.8.2.
   class PoseidonAdapter
+    # @yield configure poseidon in the yielded block
+    #   The following configuration is mandatory:
+    #   * broker_list: list of Kafka broker addresses
+    #   * zookeeper_list: list of Zookeeper addresses
+    #   * topic: name of the topic to subscribe to
+    #   * group: name of the consumer group
+    #
+    #   Any other configuration value is passed to
+    #   {http://www.rubydoc.info/github/bsm/poseidon_cluster/Poseidon/ConsumerGroup Poseidon::ConsumerGroup}.
     def initialize
       @poseidon_consumer = nil
       @closed = false
@@ -15,11 +26,16 @@ module GlassOctopus
       validate_options!
     end
 
+    # Connect to Kafka and Zookeeper, register the consumer group.
+    # This also initates a rebalance in the consumer group.
     def connect
       @poseidon_consumer = create_consumer_group
       self
     end
 
+    # Fetch messages from kafka in a loop.
+    # @yield messages read from Kafka
+    # @yieldparam message [Message] a Kafka message
     def fetch_message
       @poseidon_consumer.fetch_loop do |partition, messages|
         break if closed?
@@ -33,15 +49,18 @@ module GlassOctopus
       end
     end
 
+    # @api private
     def close
       @closed = true
       @poseidon_consumer.close if @poseidon_consumer
     end
 
+    # @api private
     def closed?
       @closed
     end
 
+    # @api private
     def create_consumer_group
       options = @options.dup
 
@@ -54,10 +73,12 @@ module GlassOctopus
       )
     end
 
+    # @api private
     def build_message(partition, message)
       GlassOctopus::Message.new(message.topic, partition, message.offset, message.key, message.value)
     end
 
+    # @api private
     def validate_options!
       errors = []
       [:group, :broker_list, :zookeeper_list, :topic].each do |key|
