bf5d7cf07f524d9afd055ea63cfa4709f95392c0
Dori Czapari <dori.czapari@gmail.com>
2018-02-08 11:47:06 +0100

    Add tests for AvroParser

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ require "minitest/autorun"
 require "minitest/color"
 require "minitest/guard_minitest_plugin"
 require "support/docker"
+require "sinatra"
+require "webmock/minitest"
 
 lib_path = File.expand_path("../../lib", __FILE__)
 $LOAD_PATH.unshift(lib_path) unless $LOAD_PATH.include?(lib_path)
